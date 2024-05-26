55602344bb57049be7ff6204699f0b8577ea47b4
VarroBence <varrobence17@gmail.com>
2024-05-26 21:28:20 +0200

    HomePage form és osztály létrehozása

---
+++ AlbumShop/src/Main.java
@@ -0,0 +1,7 @@
+import Pages.*;
+
+public class Main {
+    public static void main(String[] args) {
+        HomePage homePage = new HomePage(null);
+    }
+}
---
+++ AlbumShop/src/Pages/HomePage.java
@@ -0,0 +1,41 @@
+package Pages;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HomePage extends JDialog{
+    private JPanel homePanel;
+    private JButton loginBtn;
+    private JButton registBtn;
+
+    public HomePage(JFrame parent){
+        super(parent);
+        setTitle("Kezdőoldal");
+        setContentPane(homePanel);
+        setMinimumSize(new Dimension(450, 500));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setForeground(Color.white);
+
+        loginBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                Login login = new Login();
+                login.showPage();
+            }
+        });
+
+        registBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                CreateUser createUser = new CreateUser(null);
+                createUser.setVisible(true);
+            }
+        });
+    }
+}
