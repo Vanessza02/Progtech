03f1ccc8224fc7eb4f490f7d730cdf4871467183
Vanessza02 <114110363+Vanessza02@users.noreply.github.com>
2024-05-27 18:10:47 +0200

    login megj.+ kep hozzairasa

--- AlbumShop/src/Pages/HomePage.java
+++ AlbumShop/src/Pages/HomePage.java
@@ -1,16 +1,19 @@
 package Pages;
-
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class HomePage extends JDialog{
     private JPanel homePanel;
     private JButton loginBtn;
     private JButton registBtn;
-
-    public HomePage(JFrame parent){
+    private  JLabel kep;
+    
+    public HomePage(JFrame parent) throws IOException{
         super(parent);
         setTitle("Kezdőoldal");
         setContentPane(homePanel);
@@ -20,11 +23,20 @@ public class HomePage extends JDialog{
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setForeground(Color.white);
 
+         File imageFile = new File("C:\\Users\\janos\\Downloads\\Progtech-main\\AlbumShop\\image.jpg");
+        Image img = ImageIO.read(imageFile);
+        Image scaledImg = img.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        kep.setIcon(new ImageIcon(scaledImg));
+        
         loginBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose();
-                Login login = new Login();
+                Login login = null;
+                try {
+                    login = new Login(null);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
                 login.showPage();
             }
         });
