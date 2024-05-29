a6c2d9ae5d6ccf1d5405e0cae44751740c0b8b24
Vanessza02 <114110363+Vanessza02@users.noreply.github.com>
2024-05-29 18:02:33 +0200

    Create album form

---
+++ AlbumShop/src/Pages/CreateAlbum.java
@@ -0,0 +1,12 @@
+package Pages;
+
+import javax.swing.*;
+
+public class CreateAlbum {
+    private JPanel mainPanel;
+    private JButton létrehozásButton;
+    private JTextField artisttext;
+    private JTextField ttitletext;
+    private JTextField genretext;
+    private JTextField pricetext;
+}
