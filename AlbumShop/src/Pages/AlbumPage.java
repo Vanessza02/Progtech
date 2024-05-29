afec95522031a07988a626e6c15ceff0739e581f
Vanessza02 <114110363+Vanessza02@users.noreply.github.com>
2024-05-29 18:48:47 +0200

    Album page

---
+++ AlbumShop/src/Pages/AlbumPage.java
@@ -0,0 +1,11 @@
+package Pages;
+
+import javax.swing.*;
+
+public class AlbumPage {
+    private JPanel mainPanel;
+    private JButton updatebutt;
+    private JButton deletebutt;
+    private JTable albumTable;
+    private JScrollPane dataPanel;
+}
